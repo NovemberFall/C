5f0173e4aa6d0d5897eee62bf0b456dde0e17254
Git <1219.zheng@gmail.com>
2020-01-08 10:41:52 -0800

    pointer

--- pointer/Arrary_of_pointers/part1.c
+++ pointer/Arrary_of_pointers/part1.c
@@ -1,2 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h> //malloc, calloc, realloc
+int main()
+{
+    /**
+     * p is array of pointer
+     * each element of array p 
+     * is integer pointer
+    */
+    int *p[3];
+    int **k;
+    int x[] = {1, 2, 3};
+    int y[] = {10, 20, 30};
+    int z[] = {5, 6, 7};
+
+    p[0] = x;
+    p[1] = y;
+    p[2] = z;
+
+    return 0;
+}
